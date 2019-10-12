476fd10fd2a777f653e55e36200a541194fb3214
Abidrickthy <37216716+Abidrickthy@users.noreply.github.com>
2019-10-12 16:30:11 +0600

    Added A-Very-Big-Sum

---
+++ index.js
@@ -0,0 +1,10 @@
+function aVeryBigSum(ar) {
+  var sum = 0;
+  for (var i = 0; i < ar.length; i++){
+    sum += ar[i];
+  }
+  return sum;
+
+}
+
+aVeryBigSum([1000000001,1000000002,1000000003,1000000004 ,1000000005])
